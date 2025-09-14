003290cb14d6d0b23dc788dff491016944a48f44
theoplegends <theoplegendsalt1@gmail.com>
2025-09-14 14:15:00 +0300

    more flexible ui design

--- src/main/java/star/sequoia2/gui/screen/BetterGuildMapScreen.java
+++ src/main/java/star/sequoia2/gui/screen/BetterGuildMapScreen.java
@@ -15,22 +15,52 @@ import com.wynntils.utils.type.BoundingBox;
 import mil.nga.color.Color;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 import star.sequoia2.accessors.RenderUtilAccessor;
 import star.sequoia2.utils.render.TextureStorage;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Objects;
 
 public class BetterGuildMapScreen extends AbstractMapScreen implements RenderUtilAccessor {
-    boolean optedIn = false;
+    enum Roles { TANK, DPS, HEAL }
+    enum UiState { OPTED_OUT, CHOOSING_ROLE, CHOSEN_ROLE }
 
+    static final float SCALE = 1f;
+    static final float PAD = 6f * SCALE;
+    static final float BTN_SIZE = 46f * SCALE;
+    static final float BTN_GAP = 8f * SCALE;
+    static final float BAR_HEIGHT = BTN_SIZE + PAD * 2f;
+    static final float BAR_CORNER = 6f * SCALE;
+
+    static final float OPT_BTN_SIZE = 42f * SCALE;
+    static final float OPT_CORNER = 5f * SCALE;
+
+    static final float OPT_OUT_SIZE = 20f * SCALE;
+
+    static class Rect {
+        final float x, y, w, h;
+        Rect(float x, float y, float w, float h) { this.x = x; this.y = y; this.w = w; this.h = h; }
+        boolean contains(double mx, double my) { return mx >= x && mx <= x + w && my >= y && my <= y + h; }
+    }
+
+    static class TexturePair {
+        final Identifier active, inactive;
+        TexturePair(Identifier active, Identifier inactive) { this.active = active; this.inactive = inactive; }
+    }
+
+    final EnumMap<Roles, TexturePair> roleTextures = new EnumMap<>(Roles.class);
+
+    UiState state = UiState.OPTED_OUT;
     Roles selectedRole = null;
 
-    enum Roles {
-        TANK,
-        DPS,
-        HEAL
+    public BetterGuildMapScreen() {
+        roleTextures.put(Roles.TANK, new TexturePair(TextureStorage.tank_active, TextureStorage.tank_inactive));
+        roleTextures.put(Roles.DPS, new TexturePair(TextureStorage.damage_active, TextureStorage.damage_inactive));
+        roleTextures.put(Roles.HEAL, new TexturePair(TextureStorage.healer_active, TextureStorage.healer_inactive));
     }
 
     @Override
@@ -40,8 +70,6 @@ public class BetterGuildMapScreen extends AbstractMapScreen implements RenderUti
         RenderSystem.enableDepthTest();
         renderMap(context);
 
-
-
         RenderUtils.enableScissor(
                 context,
                 (int) (renderX + renderedBorderXOffset),
@@ -50,96 +78,82 @@ public class BetterGuildMapScreen extends AbstractMapScreen implements RenderUti
                 (int) mapHeight);
 
         renderPois(context.getMatrices(), mouseX, mouseY);
-
         RenderUtils.disableScissor(context);
 
-        float renderPosX = renderX + renderedBorderXOffset;
-        float renderPosY = renderY + renderedBorderYOffset;
+        drawUi(context, mouseX, mouseY);
+    }
 
-        if (!optedIn) {
-            render2DUtil().roundRectFilled(context.getMatrices(), renderPosX + 5, renderPosY + 5, renderPosX + 50, renderY + renderedBorderYOffset + 50, 5f, Color.darkGray());
-            render2DUtil().drawTexture(context, TextureStorage.opt_in, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-        } else if (selectedRole == null) {
-            render2DUtil().roundRectFilled(context.getMatrices(), renderPosX, renderPosY + 5, renderPosX + (50*3), renderPosY + 50, 5f, Color.darkGray());
+    void drawUi(DrawContext ctx, int mouseX, int mouseY) {
+        float baseX = renderX + renderedBorderXOffset;
+        float baseY = renderY + renderedBorderYOffset;
 
-            render2DUtil().drawTexture(context, TextureStorage.tank_inactive, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-            renderPosX = renderPosX + 50;
-            render2DUtil().drawTexture(context, TextureStorage.damage_inactive, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-            renderPosX = renderPosX + 50;
-            render2DUtil().drawTexture(context, TextureStorage.healer_inactive, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-        } else {
-            switch (selectedRole) {
-                case TANK:
-                    render2DUtil().roundRectFilled(context.getMatrices(), renderPosX, renderPosY + 5, renderPosX + (50*3), renderPosY + 50, 5f, Color.darkGray());
-
-                    render2DUtil().drawTexture(context, TextureStorage.tank_active, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-                    renderPosX = renderPosX + 50;
-                    render2DUtil().drawTexture(context, TextureStorage.damage_inactive, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-                    renderPosX = renderPosX + 50;
-                    render2DUtil().drawTexture(context, TextureStorage.healer_inactive, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-                    render2DUtil().roundRectFilled(context.getMatrices(), renderPosX + 45, renderPosY, renderPosX + 45 + 20, renderPosY + 20, 1f, Color.gray());
-                    render2DUtil().drawTexture(context, TextureStorage.opt_out, renderPosX + 45, renderPosY, renderPosX + 45 + 18, renderPosY + 18);
-                    return;
-                case DPS:
-                    render2DUtil().roundRectFilled(context.getMatrices(), renderPosX, renderPosY + 5, renderPosX + (50*3), renderPosY + 50, 5f, Color.darkGray());
-
-                    render2DUtil().drawTexture(context, TextureStorage.tank_inactive, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-                    renderPosX = renderPosX + 50;
-                    render2DUtil().drawTexture(context, TextureStorage.damage_active, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-                    renderPosX = renderPosX + 50;
-                    render2DUtil().drawTexture(context, TextureStorage.healer_inactive, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-                    render2DUtil().roundRectFilled(context.getMatrices(), renderPosX + 45, renderPosY, renderPosX + 45 + 20, renderPosY + 20, 1f, Color.gray());
-                    render2DUtil().drawTexture(context, TextureStorage.opt_out, renderPosX + 45, renderPosY, renderPosX + 45 + 18, renderPosY + 18);
-                    return;
-                case HEAL:
-                    render2DUtil().roundRectFilled(context.getMatrices(), renderPosX, renderPosY + 5, renderPosX + (50*3), renderPosY + 50, 5f, Color.darkGray());
-
-                    render2DUtil().drawTexture(context, TextureStorage.tank_inactive, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-                    renderPosX = renderPosX + 50;
-                    render2DUtil().drawTexture(context, TextureStorage.damage_inactive, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-                    renderPosX = renderPosX + 50;
-                    render2DUtil().drawTexture(context, TextureStorage.healer_active, renderPosX + 10, renderPosY + 10, renderPosX + 45, renderPosY + 45);
-                    render2DUtil().roundRectFilled(context.getMatrices(), renderPosX + 45, renderPosY, renderPosX + 45 + 20, renderPosY + 20, 1f, Color.gray());
-                    render2DUtil().drawTexture(context, TextureStorage.opt_out, renderPosX + 45, renderPosY, renderPosX + 45 + 18, renderPosY + 18);
-                    return;
-            }
+        if (state == UiState.OPTED_OUT) {
+            Rect r = new Rect(baseX + PAD, baseY + PAD, OPT_BTN_SIZE, OPT_BTN_SIZE);
+            render2DUtil().roundRectFilled(ctx.getMatrices(), r.x, r.y, r.x + r.w, r.y + r.h, OPT_CORNER, Color.darkGray());
+            render2DUtil().drawTexture(ctx, TextureStorage.opt_in, r.x + 4, r.y + 4, r.x + r.w - 4, r.y + r.h - 4);
+            return;
         }
 
+        float barWidth = PAD + (BTN_SIZE * 3f) + (BTN_GAP * 2f) + PAD;
+        Rect bar = new Rect(baseX, baseY + PAD, barWidth, BAR_HEIGHT);
+        render2DUtil().roundRectFilled(ctx.getMatrices(), bar.x, bar.y, bar.x + bar.w, bar.y + bar.h, BAR_CORNER, Color.darkGray());
+
+        float bx = bar.x + PAD;
+        Rect tankRect = new Rect(bx, bar.y + PAD, BTN_SIZE, BTN_SIZE);
+        Rect dpsRect = new Rect(bx + BTN_SIZE + BTN_GAP, bar.y + PAD, BTN_SIZE, BTN_SIZE);
+        Rect healRect = new Rect(bx + (BTN_SIZE + BTN_GAP) * 2f, bar.y + PAD, BTN_SIZE, BTN_SIZE);
+
+        drawRoleButton(ctx, tankRect, Roles.TANK);
+        drawRoleButton(ctx, dpsRect, Roles.DPS);
+        drawRoleButton(ctx, healRect, Roles.HEAL);
+
+        if (state != UiState.OPTED_OUT) {
+            Rect out = new Rect(healRect.x + healRect.w - 1 + 4, bar.y - 4, OPT_OUT_SIZE, OPT_OUT_SIZE);
+            render2DUtil().roundRectFilled(ctx.getMatrices(), out.x, out.y, out.x + out.w, out.y + out.h, 2f, Color.gray());
+            render2DUtil().drawTexture(ctx, TextureStorage.opt_out, out.x + 1, out.y + 1, out.x + out.w - 1, out.y + out.h - 1);
+        }
+    }
+
+    void drawRoleButton(DrawContext ctx, Rect r, Roles role) {
+        boolean active = Objects.equals(selectedRole, role);
+        TexturePair tp = roleTextures.get(role);
+        render2DUtil().drawTexture(ctx, active ? tp.active : tp.inactive, r.x, r.y, r.x + r.w, r.y + r.h);
     }
 
     @Override
     public boolean doMouseClicked(double mouseX, double mouseY, int button) {
-        float renderPosX = renderX + renderedBorderXOffset;
-        float renderPosY = renderY + renderedBorderYOffset;
-        if (button == GLFW.GLFW_MOUSE_BUTTON_LEFT) {
-            if (isWithin(mouseX, mouseY, renderPosX + 5, renderPosY + 5, renderPosX + 50, renderPosY + 50) && !optedIn) {
-                optedIn = true;
-            } else if (optedIn && selectedRole == null) {
-                if (isWithin(mouseX, mouseY, renderPosX + 5, renderPosY + 5, renderPosX + 50, renderPosY + 50)) {
-                    selectedRole = Roles.TANK;
-                }
-                renderPosX = renderPosX + 50;
-                if (isWithin(mouseX, mouseY, renderPosX + 5, renderPosY + 5, renderPosX + 50, renderPosY + 50)) {
-                    selectedRole = Roles.DPS;
-                }
-                renderPosX = renderPosX + 50;
-                if (isWithin(mouseX, mouseY, renderPosX + 5, renderPosY + 5, renderPosX + 50, renderPosY + 50)) {
-                    selectedRole = Roles.HEAL;
-                }
-            } else {
-                renderPosX = renderPosX + 50;renderPosX = renderPosX + 50;
-                if (isWithin(mouseX, mouseY, renderPosX + 45, renderPosY, renderPosX + 45 + 18, renderPosY + 18)) {
-                    optedIn = false;
-                    selectedRole = null;
-                }
+        if (button != GLFW.GLFW_MOUSE_BUTTON_LEFT) return super.doMouseClicked(mouseX, mouseY, button);
+
+        float baseX = renderX + renderedBorderXOffset;
+        float baseY = renderY + renderedBorderYOffset;
+
+        if (state == UiState.OPTED_OUT) {
+            Rect optIn = new Rect(baseX + PAD, baseY + PAD, OPT_BTN_SIZE, OPT_BTN_SIZE);
+            if (optIn.contains(mouseX, mouseY)) {
+                state = UiState.CHOOSING_ROLE;
+                selectedRole = null;
+                return true;
             }
+            return super.doMouseClicked(mouseX, mouseY, button);
+        }
+
+        float barWidth = PAD + (BTN_SIZE * 3f) + (BTN_GAP * 2f) + PAD;
+        Rect bar = new Rect(baseX, baseY + PAD, barWidth, BAR_HEIGHT);
+
+        Rect tankRect = new Rect(bar.x + PAD, bar.y + PAD, BTN_SIZE, BTN_SIZE);
+        Rect dpsRect = new Rect(bar.x + PAD + BTN_SIZE + BTN_GAP, bar.y + PAD, BTN_SIZE, BTN_SIZE);
+        Rect healRect = new Rect(bar.x + PAD + (BTN_SIZE + BTN_GAP) * 2f, bar.y + PAD, BTN_SIZE, BTN_SIZE);
+
+        if (tankRect.contains(mouseX, mouseY)) { selectedRole = Roles.TANK; state = UiState.CHOSEN_ROLE; return true; }
+        if (dpsRect.contains(mouseX, mouseY)) { selectedRole = Roles.DPS; state = UiState.CHOSEN_ROLE; return true; }
+        if (healRect.contains(mouseX, mouseY)) { selectedRole = Roles.HEAL; state = UiState.CHOSEN_ROLE; return true; }
+
+        if (state != UiState.OPTED_OUT) {
+            Rect out = new Rect(healRect.x + healRect.w - 1 + 4, bar.y - 4, OPT_OUT_SIZE, OPT_OUT_SIZE);
+            if (out.contains(mouseX, mouseY)) { state = UiState.OPTED_OUT; selectedRole = null; return true; }
         }
 
         return super.doMouseClicked(mouseX, mouseY, button);
-    }
-
-    public boolean isWithin(double mouseX, double mouseY, double x, double y, double x2, double y2) {
-        return mouseX >= x && mouseX <= x2 && mouseY >= y && mouseY <= y2;
     }
 
     protected void renderMap(DrawContext context) {
@@ -152,7 +166,6 @@ public class BetterGuildMapScreen extends AbstractMapScreen implements RenderUti
                 (int) mapWidth,
                 (int) mapHeight);
 
-        // Background black void color
         RenderUtils.drawRect(
                 poseStack,
                 CommonColors.BLACK,
@@ -163,7 +176,6 @@ public class BetterGuildMapScreen extends AbstractMapScreen implements RenderUti
                 mapHeight);
 
         BoundingBox textureBoundingBox = BoundingBox.centered(mapCenterX, mapCenterZ, width / zoomRenderScale, height / zoomRenderScale);
-
         List<MapTexture> maps = Services.Map.getMapsForBoundingBox(textureBoundingBox);
 
         for (MapTexture map : maps) {
@@ -184,23 +196,16 @@ public class BetterGuildMapScreen extends AbstractMapScreen implements RenderUti
         }
 
         BUFFER_SOURCE.draw();
-
         RenderUtils.disableScissor(context);
     }
 
     private void renderPois(MatrixStack matrixStack, int mouseX, int mouseY) {
         List<TerritoryPoi> advancementPois = Models.Territory.getTerritoryPoisFromAdvancement();
-
         List<Poi> renderedPois = new ArrayList<>();
 
         for (TerritoryPoi poi : advancementPois) {
             TerritoryProfile territoryProfile = Models.Territory.getTerritoryProfile(poi.getName());
-
-            // If the API and advamcement pois don't match, we use the API pois without advancement info
-            if (territoryProfile != null
-                    && territoryProfile
-                    .getGuild()
-                    .equals(poi.getTerritoryInfo().getGuildName())) {
+            if (territoryProfile != null && territoryProfile.getGuild().equals(poi.getTerritoryInfo().getGuildName())) {
                 renderedPois.add(poi);
             } else {
                 renderedPois.add(new TerritoryPoi(territoryProfile, poi.getTerritoryInfo()));
